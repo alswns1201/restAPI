6925fa19f6199a73a3564a6c4c887431508d9e69
kkmjj <alswns1201@gmail.com>
2024-11-17 12:32:01 +0900

    Cookies에 토큰 담아서 이동 페이지에 적용. (universal-cookie 이용 )

--- vite-project/js/api.js
+++ vite-project/js/api.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Cookies from "universal-cookie";
 
 export  const testApi = ()=>{
     console.log("test api");
@@ -6,10 +7,18 @@ export  const testApi = ()=>{
 
 const url ="http://localhost:8080/api/v1/";
 
-export const makeToken =(mid,mpw)=>{
+export const makeToken = async (mid,mpw)=>{
     const path = url +"token/make"
 
     const data = {mid,mpw}
 
-    axios.post(path,data);
+    const res = await axios.post(path,data);
+
+    return res.data;
 }
+
+const cookies = new Cookies(null, {path:'/',maxAge :2592000});
+
+export const saveToken =(tokenName,tokenValue)=>{
+    cookies.set(tokenName,tokenValue);
+};
